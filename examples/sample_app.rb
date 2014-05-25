e68fc37607b76d0749069c2f215c86df297faf8f
Kyuden <msmsms.um@gmail.com>
2014-05-26 02:46:00 +0900

    Delte debug code

--- examples/sample_app.rb
+++ examples/sample_app.rb
@@ -1,8 +1,3 @@
-lib = File.expand_path('./lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "pry"
-require "pry-debugger"
-require "pp"
 require 'salen'
 
 class SampleApp < Salen::App
